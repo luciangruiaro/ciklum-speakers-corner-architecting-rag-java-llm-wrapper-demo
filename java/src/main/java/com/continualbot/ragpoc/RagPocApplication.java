e4f6cf959d13ef4c0a1937a9b145344c4fe3e995
luciangruiaro <luciancgruia@gmail.com>
2024-03-24 21:11:20 +0200

    aligned with step 2

--- java/src/main/java/com/continualbot/ragpoc/RagPocApplication.java
+++ java/src/main/java/com/continualbot/ragpoc/RagPocApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RagPocApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RagPocApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RagPocApplication.class, args);
+    }
 
 }
