96c1f559e68536295f78d09f50b22547a61292c1
luciangruiaro <luciancgruia@gmail.com>
2024-03-24 15:08:33 +0200

    initial commit

---
+++ java/src/main/java/com/continualbot/ragpoc/RagPocApplication.java
@@ -0,0 +1,13 @@
+package com.continualbot.ragpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagPocApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/com/continualbot/ragpoc/RagPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.continualbot.ragpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
